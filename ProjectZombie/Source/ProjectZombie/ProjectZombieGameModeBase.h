5397298b251e9b812da5825c474473329c167ce0
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-02-04 22:08:49 +0000

    Added ProjectZombie Unreal project.

---
+++ ProjectZombie/Source/ProjectZombie/ProjectZombieGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectZombieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZOMBIE_API AProjectZombieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
