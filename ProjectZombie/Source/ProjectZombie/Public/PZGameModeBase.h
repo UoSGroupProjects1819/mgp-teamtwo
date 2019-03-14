8498220149ab4682741f621bcdb6cb5514fe0328
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-03-14 20:30:51 +0000

    Added death screen.

--- ProjectZombie/Source/ProjectZombie/Public/PZGameModeBase.h
+++ ProjectZombie/Source/ProjectZombie/Public/PZGameModeBase.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "PZGameModeBase.generated.h"
 
 UCLASS()
-class PROJECTZOMBIE_API APZGameModeBase : public AGameModeBase
+class PROJECTZOMBIE_API APZGameModeBase : public AGameMode
 {
 	GENERATED_BODY()
 	
